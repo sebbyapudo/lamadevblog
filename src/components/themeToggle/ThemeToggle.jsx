7afbeeb8b588886d35d1b3818abee6a075cf5c28
sebbyapudo <sebbyapudo@gmail.com>
2025-11-25 22:03:55 +0300

    Fixed the theme switch functionality

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -13,9 +13,20 @@ const ThemeToggle = () => {
   
 
   return (
-    <div className={styles.container} onClick={toggle}>
+    <div className={styles.container} onClick={toggle} 
+      style={
+          theme === "dark" ? {left:1,backgroundColor: "white"} : 
+          {right: 1, backgroundColor: "#0f172a"}
+          }
+    >
       <Image src="/moon.png" alt='' height={14} width={14}/>
-      <div className={styles.ball}></div>
+      <div className={styles.ball} 
+        style={
+          theme === "dark" ? {left:1,backgroundColor: "0f172a"} : 
+          {right: 1, backgroundColor: "#ffffff"}
+          }>
+
+      </div>
       <Image src="/sun.png" alt='' height={14} width={14}/>
     </div>
   )
