ce370a312836e4ff746e99ee6444d4901d17c4ef
sebbyapudo <sebbyapudo@gmail.com>
2025-11-26 09:11:01 +0300

    Set up the layout for the Featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey there!</b> This is a Marxist Leninist Blog!
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt='' fill className={styles.image}/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Cupiditate dicta laborum quasi deleniti fugit expedita!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
